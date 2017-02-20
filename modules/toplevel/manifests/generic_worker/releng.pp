eb6b901dd74bf739ed051c325eabd2085d65743b
Pete Moore <pmoore@mozilla.com>
2017-02-20 19:11:37 +0100

    Make t-yosemite-r7-0049.test.releng.scl3.mozilla.com run generic-worker rather than taskcluster-worker

--- modules/toplevel/manifests/generic_worker/releng.pp
+++ modules/toplevel/manifests/generic_worker/releng.pp
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-class toplevel::worker::releng inherits toplevel::worker {
+class toplevel::generic_worker::releng inherits toplevel::worker {
     include instance_metadata
     include clean::appstate
 
