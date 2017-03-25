0a600f2a1c2f92f9d763f8fa19a7a8cac80c06c4
Pete Moore <pmoore@mozilla.com>
2017-03-25 16:11:23 +0100

    renamed tc creds secrets for generic-worker

--- modules/generic_worker/manifests/init.pp
+++ modules/generic_worker/manifests/init.pp
@@ -4,8 +4,8 @@ class generic_worker {
     case $::operatingsystem {
         Darwin: {
             $macos_version = regsubst($::macosx_productversion_major, '\.', '-')
-            $taskcluster_client_id = secret('generic_worker_client_id')
-            $taskcluster_access_token = hiera('generic_worker_access_token')
+            $taskcluster_client_id = secret('generic_worker_macosx_client_id')
+            $taskcluster_access_token = hiera('generic_worker_macosx_access_token')
 
             file { '/Library/LaunchAgents/net.generic.worker.plist':
                 ensure => present,
