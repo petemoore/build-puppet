02c50249fe885a04e3bc43d8c978af737984ea44
Pete Moore <pmoore@mozilla.com>
2017-03-25 20:46:33 +0100

    fix livelog secret

--- modules/generic_worker/manifests/init.pp
+++ modules/generic_worker/manifests/init.pp
@@ -6,6 +6,7 @@ class generic_worker {
             $macos_version = regsubst($::macosx_productversion_major, '\.', '-')
             $taskcluster_client_id = secret('generic_worker_macosx_client_id')
             $taskcluster_access_token = hiera('generic_worker_macosx_access_token')
+            $livelog_secret = hiera('livelog_secret')
 
             file { '/Library/LaunchAgents/net.generic.worker.plist':
                 ensure => present,
